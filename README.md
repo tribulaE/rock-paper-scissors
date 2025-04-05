7dc6e5abf057b75bfbb557402a53a2bc5333d5c1
tribulaE <198527537+tribulaE@users.noreply.github.com>
2025-04-05 11:02:15 -0400

    I finished the game

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # rock-paper-scissors
+
+I will be creating a rock-paper-scissors game, with the odin project.
---
+++ script.js
@@ -0,0 +1,103 @@
+//Game played against computer//
+//Write a function that at random return rock, paper or scissors//
+
+//1. Create a new function called getComputerChoice//
+//2. getComputerChoice will randomly return rock paper or scissors//
+
+const help = ["rock", "paper", "scissors"]
+
+function getComputerChoice() {
+    return help[Math.floor(Math.random() * help.length)];
+}
+    
+
+//Create a Function getHumanChoice//
+//Create a prompt//
+//Use return method in the function//
+
+    function getHumanChoice() {
+        return prompt("rock, paper scissors");
+    }
+    
+//Declare the players score variable//
+//Create two variable human score and computerScore
+//Make them equal 0//
+
+
+
+//Create function playRound//S
+//Make two parameters fo playRound and HumanChoice and computeChoice//
+//Make humanChoice case-insensitive//
+//write a function playGame//
+//move playRound into playGame//
+//call playRound 5 times//
+
+ 
+function playGame() { 
+
+let humanScore = 0;
+let computerScore = 0;
+
+
+    function playRound(humanChoice, computerChoice) {
+        humanChoice = humanChoice.toLowerCase();
+
+        if (humanChoice == computerChoice)  {
+            console.log("Its a tie!!!")
+        } else if (humanChoice == "rock" && computerChoice == "scissors" || humanChoice == "paper" && computerChoice == "rock" || humanChoice == "scissors" && computerChoice == "paper"  ){
+            console.log("You win! You deserve a cookie")
+            humanScore++;
+            console.log("You have a score of " + humanScore);
+
+
+        }   else if (humanChoice == "paper" && computerChoice == "scissors" || humanChoice == "rock" && computerChoice == "paper" || humanChoice == "scissors" && computerChoice == "rock")  {
+            console.log("You lose! BOOOO")
+            computerScore++;
+            console.log( "The computer has a score of " + computerScore )
+        }   else {
+            console.log("YOU'RE NOT PLAYING THE GAME")
+        }
+
+        
+        }
+
+     for (let round = 0; round < 5; round++) {
+        const humanChoice = getHumanChoice();
+        const computerChoice = getComputerChoice();
+        
+        
+        playRound(humanChoice, computerChoice)
+
+       
+    }   if (humanScore > computerScore) {
+        console.log("CONGRATS YOU WIN!! SCOREBOARD FOR YOU IS " + humanScore + " SCOREBOARD FOR COMPUTER IS "+ computerScore);
+    }   else if (humanScore < computerScore) {
+        console.log("YOU LOSE!! SCOREBOARD FOR COMPUTER" + computerScore + " SCOREBOARD FOR YOU " +  humanScore)
+    } else if(huamnScore == computerScore) {
+        console.log("its a tie")
+    }
+         
+          
+    }
+       
+
+        
+    
+        
+           
+
+         
+playGame();
+
+
+
+
+  
+
+
+  
+
+  
+ 
+  
+
